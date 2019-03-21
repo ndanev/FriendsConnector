51c654d1d1878bd2513ebeec3371e9aaaa0b8fa2
Nemanja Danev <nemanja.danev.93@gmail.com>
2019-03-21 08:35:35 -0400

    changed style in show all users

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 
+
+// ============== //
+// SHOW ALL USERS //
+// ============== //
+
 app.get('/users', function(req, res) {
-
-    // var users = JSON.stringify(data);
-
-    // res.header("Content-Type",'application/json');
-
 
     res.render('users', { users: users});
 
@@ -33,20 +33,19 @@ app.get('/show_friends/:id', function(req, res) {
 
     var friends = users[4].friends;
     var friendList = [];
+    
 
     // console.log(friends);
 
     for (i in friends) {
 
-        friendList.push(users[friends[i]-1].firstName + " " + users[friends[i]-1].surname);
+        friendList.push(users[friends[i] -1 ].firstName + " " + users[friends[i] -1 ].surname);
 
     }
 
     res.render('friends', {users: users, friendList: friendList});
 
 });
-
-
 
 
 
